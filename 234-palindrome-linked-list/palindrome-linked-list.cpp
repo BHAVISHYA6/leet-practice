8e064e7980846b7493194d688ae4e746e1c310e6
Bhavishya C <bhavi5002@gmail.com>
2026-07-13 20:24:34 +0530

    Time: 16 ms (5.78%) | Memory: 122 MB (38.43%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,21 +10,42 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        stack<int> st;
-        ListNode* temp = head;
-        while(temp!= NULL){
-            st.push(temp->val);
-            temp = temp->next;
+    ListNode* reverseLinkedList(ListNode* head){
+        if(head == NULL || head->next==NULL){
+            return head;
         }
-        temp = head;
+        ListNode* temp = head;
+        ListNode* prev = NULL;
         while(temp != NULL){
-            if(temp->val != st.top()){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp ;
+            temp = front ;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return true;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newHead = reverseLinkedList(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second != NULL){
+            if(first->val != second->val){
+                reverseLinkedList(newHead);
                 return false;
             }
-            temp = temp->next;
-            st.pop();
+            first = first->next;
+            second = second->next;
         }
+        reverseLinkedList(newHead);
         return true;
         
     }
