82746f1d7bd8a1093509b190e2b752615d54b303
Bhavishya C <bhavi5002@gmail.com>
2026-08-21 16:46:04 +0530

    Time: 24 ms (14.85%) | Memory: 40.4 MB (7.46%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int, int> mp;
+        mp[0] =1;
+        int presum =0 , cnt =0;
+        for(int i =0 ; i< nums.size(); i++){
+            presum += nums[i];
+            int remove = presum -goal;
+            cnt += mp[remove];
+            mp[presum] += 1;
+        }
+        return cnt ;
+    }
+};
