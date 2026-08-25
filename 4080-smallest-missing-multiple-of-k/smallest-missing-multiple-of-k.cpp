a3a0e8f26605e4426e294435d7327311cdde4db9
Bhavishya C <bhavi5002@gmail.com>
2026-08-25 11:25:29 +0530

    Time: 1 ms (47.29%) | Memory: 24.2 MB (67.36%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_map<int , int> mp;
-        for(int i =0 ; i< nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        bool flag = true;
-        int cnt = 1;
-        while(flag){
-            if(mp.find(cnt*k) != mp.end()){
-                cnt++;
-            }else{
-                flag = false;
+        sort(nums.begin(), nums.end());
+        int cnt =1 ;
+        for(int i=0 ; i< nums.size(); i++){
+            if((cnt*k) == nums[i]){
+            cnt++;
             }
         }
         return cnt*k;
