77d976dd5be004949e779e3322a7b604d2331670
Bhavishya C <bhavi5002@gmail.com>
2026-09-03 17:17:42 +0530

    Time: 8 ms (47.38%) | Memory: 166 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int oddc =0 , evenc = 0;
-        bool fails = true;
-        sort(nums1.begin(), nums1.end());
-      for(int i =0 ; i< nums1.size(); i++){
-            if(nums1[i] %2 == 0){
-                if(oddc == 0) {
-                    fails = false;
-                }
-                evenc++;
-            }else{
-                oddc++;
-            }
-      }  
-      if(oddc == nums1.size() || evenc == nums1.size()) return true;
-      if(fails == true){
-        return true;
-      }
-      return false;
+        int mini = INT_MAX, evenc =0 , oddc =0;
+        for(int i =0 ; i< nums1.size(); i++){
+            if(nums1[i]%2 == 0) evenc++;
+            else oddc++;
+            if(nums1[i] < mini) mini = nums1[i];
+        }
+        if(oddc == nums1.size() || evenc == nums1.size()) return true;
+        if(mini %2 == 1) return true;
+        return false;
     }
 };
